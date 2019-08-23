dc86f647277c6a6f02a917da8eaf1a39991491af
Ani Kerr <ani.kerr@gmail.com>
2019-08-23 15:50:07 -0400

    styling

--- App.js
+++ App.js
@@ -9,13 +9,13 @@
 
 import React, { Component } from 'react';
 import {
-   AppRegistry,
-   Text,
-   View,
-   StyleSheet,
-   PixelRatio,
-   TouchableHighlight,
-   Image,
+  AppRegistry,
+  Text,
+  View,
+  StyleSheet,
+  PixelRatio,
+  TouchableHighlight,
+  Image,
 } from 'react-native';
 
 import { secret } from './secrets.js';
@@ -26,7 +26,7 @@ import { ViroARSceneNavigator } from 'react-viro';
 const apiKey = secret.apiKey;
 
 let sharedProps = {
-   apiKey,
+  apiKey,
 };
 
 let InitialARScene = require('./js/MainScene.js');
@@ -36,223 +36,225 @@ let AR_NAVIGATOR_TYPE = 'AR';
 let defaultNavigatorType = UNSET;
 
 export default class App extends Component {
-   constructor() {
-      super();
+  constructor() {
+    super();
 
-      this.state = {
-         navigatorType: defaultNavigatorType,
-         sharedProps: sharedProps,
-         coinsCollected: 0,
-         totalCoins: 0,
-         won: false,
-         maze: [],
-      };
-   }
+    this.state = {
+      navigatorType: defaultNavigatorType,
+      sharedProps: sharedProps,
+      coinsCollected: 0,
+      totalCoins: 0,
+      won: false,
+      maze: [],
+    };
+  }
 
-   componentDidUpdate() {
-      if (!this.state.maze.length) {
-         this.getMaze();
+  componentDidUpdate() {
+    if (!this.state.maze.length) {
+      this.getMaze();
+    }
+  }
+
+  render() {
+    if (this.state.navigatorType == UNSET) {
+      return this.getHomeScreen();
+    } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
+      return this.renderAR();
+    }
+  }
+  getTotalCoins = arr => {
+    let totalCoins = 0;
+    for (let i = arr.length - 1; i >= 0; i--) {
+      for (let j = 0; j < arr[0].length; j++) {
+        if (arr[i][j] === 3) {
+          totalCoins++;
+        }
       }
-   }
+    }
+    return totalCoins;
+  };
 
-   render() {
-      if (this.state.navigatorType == UNSET) {
-         return this.getHomeScreen();
-      } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
-         return this.renderAR();
-      }
-   }
-   getTotalCoins = arr => {
-      let totalCoins = 0;
-      for (let i = arr.length - 1; i >= 0; i--) {
-         for (let j = 0; j < arr[0].length; j++) {
-            if (arr[i][j] === 3) {
-               totalCoins++;
-            }
-         }
-      }
-      return totalCoins;
-   };
+  getHomeScreen = () => {
+    return (
+      <View style={localStyles.outer}>
+        <View style={localStyles.inner}>
+          {/* <Text style={localStyles.titleText}>A Maze Thing</Text> */}
+          <Image
+            source={require('./assets/logo_sm.png')}
+            style={localStyles.logo}
+          />
+          <TouchableHighlight
+            style={localStyles.buttons}
+            onPress={this.selectScreen(AR_NAVIGATOR_TYPE)}
+            underlayColor={'#3b38ff'}
+          >
+            <Text style={localStyles.buttonText}>Random Maze</Text>
+          </TouchableHighlight>
 
-   getHomeScreen = () => {
-      return (
-         <View style={localStyles.outer}>
-            <View style={localStyles.inner}>
-               <Text style={localStyles.titleText}>A Maze Thing</Text>
+          <TouchableHighlight
+            style={localStyles.buttons}
+            onPress={() => {}}
+            underlayColor={'#3b38ff'}
+          >
+            <Text style={localStyles.buttonText}>Create a Maze</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  };
 
-               <TouchableHighlight
-                  style={localStyles.buttons}
-                  onPress={this.selectScreen(AR_NAVIGATOR_TYPE)}
-                  underlayColor={'#68a0ff'}
-               >
-                  <Text style={localStyles.buttonText}>Load Random Maze</Text>
-               </TouchableHighlight>
-
-               <TouchableHighlight
-                  style={localStyles.buttons}
-                  onPress={() => {}}
-                  underlayColor={'#68a0ff'}
-               >
-                  <Text style={localStyles.buttonText}>Create a Maze</Text>
-               </TouchableHighlight>
-            </View>
-         </View>
-      );
-   };
-
-   renderAR = () => {
-      return (
-         <View style={localStyles.container}>
-            <ViroARSceneNavigator
-               style={localStyles.arView}
-               {...this.state.sharedProps}
-               initialScene={{ scene: InitialARScene }}
-               viroAppProps={{
-                  exit: this.exitViro,
-                  incrementCoins: this.incrementCoins,
-                  setTotalCoins: this.setTotalCoins,
-                  coinsCollected: this.state.coinsCollected,
-                  won: this.state.won,
-                  win: this.win,
-                  reset: this.reset,
-                  maze: this.state.maze,
-                  totalCoins: this.state.totalCoins,
-               }}
+  renderAR = () => {
+    return (
+      <View style={localStyles.container}>
+        <ViroARSceneNavigator
+          style={localStyles.arView}
+          {...this.state.sharedProps}
+          initialScene={{ scene: InitialARScene }}
+          viroAppProps={{
+            exit: this.exitViro,
+            incrementCoins: this.incrementCoins,
+            setTotalCoins: this.setTotalCoins,
+            coinsCollected: this.state.coinsCollected,
+            won: this.state.won,
+            win: this.win,
+            reset: this.reset,
+            maze: this.state.maze,
+            totalCoins: this.state.totalCoins,
+          }}
+        />
+        <View
+          style={{
+            flex: 0.2,
+            flexDirection: 'row',
+            backgroundColor: 'transparent',
+            position: 'absolute',
+            zIndex: 2,
+            padding: 10,
+            justifyContent: 'space-between',
+            width: '100%',
+          }}
+        >
+          <TouchableHighlight onPress={this.exitViro}>
+            <Image
+              source={require('./assets/home.png')}
+              style={{ height: 40, width: 40, opacity: 0.5 }}
             />
-            <View
-               style={{
-                  flex: 0.2,
-                  backgroundColor: 'transparent',
-                  position: 'absolute',
-                  zIndex: 2,
-                  padding: 10,
-               }}
-            >
-               <TouchableHighlight onPress={this.exitViro}>
-                  <Image
-                     source={require('./assets/home.png')}
-                     style={{ height: 40, width: 40, opacity: 0.5 }}
-                  />
-               </TouchableHighlight>
-               <Text>{`${this.state.coinsCollected}/${
-                  this.state.totalCoins
-               }`}</Text>
-            </View>
-         </View>
+          </TouchableHighlight>
+          <Text style={localStyles.overlayText}>{`${
+            this.state.coinsCollected
+          }/${this.state.totalCoins}`}</Text>
+        </View>
+      </View>
+    );
+  };
+  win = () => {
+    this.setState({ won: true });
+  };
+  getMaze = async () => {
+    try {
+      const { data } = await axios.get(
+        'https://powerful-headland-69931.herokuapp.com/api/maze'
       );
-   };
-   win = () => {
-      this.setState({ won: true });
-   };
-   getMaze = async () => {
-      try {
-         const { data } = await axios.get(
-            'https://powerful-headland-69931.herokuapp.com/api/maze',
-         );
 
-         const randMaze = Math.floor(Math.random() * data.length);
+      const randMaze = Math.floor(Math.random() * data.length);
 
-         const totalCoins = this.getTotalCoins(data[randMaze].maze);
+      const totalCoins = this.getTotalCoins(data[randMaze].maze);
 
-         this.setState({
-            maze: data[randMaze].maze,
-            totalCoins,
-         });
-      } catch (err) {
-         console.error(err);
-      }
-   };
-
-   incrementCoins = () => {
-      this.setState(previousState => {
-         return {
-            coinsCollected: previousState.coinsCollected + 1,
-         };
-      });
-   };
-
-   setTotalCoins = num => {
-      this.setState({ totalCoins: num });
-   };
-
-   selectScreen = navigatorType => {
-      return () => {
-         this.setState({
-            navigatorType: navigatorType,
-         });
-      };
-   };
-
-   exitViro = () => {
       this.setState({
-         navigatorType: UNSET,
-         totalCoins: 0,
-         coinsCollected: 0,
-         maze: [],
-         won: false,
+        maze: data[randMaze].maze,
+        totalCoins,
       });
-   };
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  incrementCoins = () => {
+    this.setState(previousState => {
+      return {
+        coinsCollected: previousState.coinsCollected + 1,
+      };
+    });
+  };
+
+  setTotalCoins = num => {
+    this.setState({ totalCoins: num });
+  };
+
+  selectScreen = navigatorType => {
+    return () => {
+      this.setState({
+        navigatorType: navigatorType,
+      });
+    };
+  };
+
+  exitViro = () => {
+    this.setState({
+      navigatorType: UNSET,
+      totalCoins: 0,
+      coinsCollected: 0,
+      maze: [],
+      won: false,
+    });
+  };
 }
 
 let localStyles = StyleSheet.create({
-   container: {
-      flex: 1,
-   },
-   arView: {
-      flex: 1,
-   },
-   viroContainer: {
-      flex: 1,
-      backgroundColor: 'black',
-   },
-   outer: {
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: 'black',
-   },
-   inner: {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      backgroundColor: 'black',
-   },
-   titleText: {
-      paddingTop: 30,
-      paddingBottom: 20,
-      color: '#fff',
-      textAlign: 'center',
-      fontSize: 25,
-   },
-   buttonText: {
-      color: '#fff',
-      textAlign: 'center',
-      fontSize: 20,
-   },
-   buttons: {
-      height: 80,
-      width: 150,
-      paddingTop: 20,
-      paddingBottom: 20,
-      marginTop: 10,
-      marginBottom: 10,
-      backgroundColor: '#68a0cf',
-      borderRadius: 10,
-      borderWidth: 1,
-      borderColor: '#fff',
-   },
-   exitButton: {
-      height: 50,
-      width: 100,
-      paddingTop: 10,
-      paddingBottom: 10,
-      marginTop: 10,
-      marginBottom: 10,
-      backgroundColor: '#68a0cf',
-      borderRadius: 10,
-      borderWidth: 1,
-      borderColor: '#fff',
-   },
+  container: {
+    flex: 1,
+  },
+  arView: {
+    flex: 1,
+  },
+  viroContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  outer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  inner: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  logo: {},
+  titleText: {
+    paddingTop: 30,
+    paddingBottom: 20,
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 25,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    textAlignVertical: 'center',
+    letterSpacing: 2,
+  },
+  buttons: {
+    height: 80,
+    width: 220,
+    paddingTop: 25,
+    marginTop: 30,
+    marginBottom: 20,
+    backgroundColor: '#3734eb',
+    borderRadius: 10,
+  },
+  overlayText: {
+    alignSelf: 'flex-end',
+    color: '#fff',
+    fontWeight: '700',
+    textAlign: 'right',
+    fontSize: 16,
+    marginBottom: 15,
+  },
 });
 
 module.exports = App;
--- js/MainScene.js
+++ js/MainScene.js
@@ -4,207 +4,198 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 
 import {
-   ViroSceneNavigator,
-   ViroScene,
-   ViroARScene,
-   ViroAmbientLight,
-   Viro360Video,
-   Viro360Image,
-   ViroUtils,
-   ViroPortal,
-   ViroPortalScene,
-   Viro3DObject,
-   ViroAnimations,
-   ViroText,
-   ViroMaterials,
+  ViroSceneNavigator,
+  ViroScene,
+  ViroARScene,
+  ViroAmbientLight,
+  Viro360Video,
+  Viro360Image,
+  ViroUtils,
+  ViroPortal,
+  ViroPortalScene,
+  Viro3DObject,
+  ViroAnimations,
+  ViroText,
+  ViroMaterials,
 } from 'react-viro';
 
 let loseScreen = require('./youLose.js');
 let winScreen = require('./youWin.js');
 
 export default class MainScene extends Component {
-   constructor(props) {
-      super(props);
-      this.state = { time: 550 };
-      this.interval = null;
-   }
-   componentWillUnmount() {
+  constructor(props) {
+    super(props);
+    this.state = { time: 550 };
+    this.interval = null;
+  }
+  componentWillUnmount() {
+    this.stopTimer();
+  }
+  componentDidMount() {
+    const props = this.props.arSceneNavigator.viroAppProps;
+    if (props.totalCoins && props.totalCoins === props.coinsCollected) {
+      props.win();
+    }
+
+    if (!this.interval && this.state.time > 0) {
+      this.startTimer();
+    } else {
       this.stopTimer();
-   }
-   componentDidMount() {
-      const props = this.props.arSceneNavigator.viroAppProps;
-      if (props.totalCoins && props.totalCoins === props.coinsCollected) {
-         props.win();
-      }
+    }
+  }
 
-      if (!this.interval && this.state.time > 0) {
-         this.startTimer();
-      } else {
-         this.stopTimer();
-      }
-   }
+  startTimer = () => {
+    if (!this.interval && this.state.time > 0) {
+      this.interval = setInterval(() => {
+        this.setState({ time: this.state.time - 1 });
+        if (!this.state.time || this.props.arSceneNavigator.viroAppProps.won) {
+          this.stopTimer();
+          this.pushNextScene();
+        }
+      }, 1000);
+    } else {
+      this.stopTimer();
+    }
+  };
+  stopTimer = () => {
+    clearInterval(this.interval);
+    this.interval = null;
+  };
+  pushNextScene = () => {
+    if (!this.props.arSceneNavigator.viroAppProps.won) {
+      this.props.sceneNavigator.push({ scene: loseScreen });
+    } else {
+      this.props.sceneNavigator.push({ scene: winScreen });
+    }
+  };
 
-   startTimer = () => {
-      if (!this.interval && this.state.time > 0) {
-         this.interval = setInterval(() => {
-            this.setState({ time: this.state.time - 1 });
-            if (
-               !this.state.time ||
-               this.props.arSceneNavigator.viroAppProps.won
-            ) {
-               this.stopTimer();
-               this.pushNextScene();
-            }
-         }, 1000);
-      } else {
-         this.stopTimer();
+  mazeGenerator = () => {
+    const render = [];
+    let initialX = -5;
+    let initialZ = -2;
+    const props = this.props.arSceneNavigator.viroAppProps;
+    const arr = this.props.arSceneNavigator.viroAppProps.maze;
+    for (let i = arr.length - 1; i >= 0; i--) {
+      for (let j = 0; j < arr[0].length; j++) {
+        initialX += 1;
+        let position = [initialX, 0, initialZ];
+        if (arr[i][j] === 1 || arr[i][j] === 2) {
+          render.push(
+            <Viro3DObject
+              source={require('./res/object_cube/object_cube.vrx')}
+              resources={[
+                require('./res/object_cube/cube_diffuse.png'),
+                require('./res/object_cube/cube_specular.png'),
+              ]}
+              materials="hedge"
+              scale={[1.0, 1.0, 1.0]}
+              position={position}
+              type="VRX"
+              key={String.fromCharCode(i) + String.fromCharCode(j)}
+            />
+          );
+        } else if (arr[i][j] === 3) {
+          render.push(
+            <Viro3DObject
+              source={require('./res/coin/coin.vrx')}
+              resources={[
+                require('./res/coin/coin-texture.jpg'),
+                require('./res/coin/img1.png'),
+                require('./res/coin/img2.png'),
+                require('./res/coin/img3.png'),
+                require('./res/coin/gold.jpg'),
+              ]}
+              scale={[0.18, 0.18, 0.18]}
+              position={position}
+              type="VRX"
+              key={String.fromCharCode(i) + String.fromCharCode(j)}
+              animation={{ name: 'animateCoin', run: true, loop: true }}
+              onClickState={() => {
+                props.incrementCoins(i, j);
+                arr[i][j] = 0;
+              }}
+              onDrag={() => {
+                props.incrementCoins(i, j);
+                arr[i][j] = 0;
+              }}
+            />
+          );
+        }
       }
-   };
-   stopTimer = () => {
-      clearInterval(this.interval);
-      this.interval = null;
-   };
-   pushNextScene = () => {
-      if (!this.props.arSceneNavigator.viroAppProps.won) {
-         this.props.sceneNavigator.push({ scene: loseScreen });
-      } else {
-         this.props.sceneNavigator.push({ scene: winScreen });
-      }
-   };
+      initialX = -5;
+      initialZ--;
+    }
+    return render;
+  };
 
-   mazeGenerator = () => {
-      const render = [];
-      let initialX = -5;
-      let initialZ = -2;
-      const props = this.props.arSceneNavigator.viroAppProps;
-      const arr = this.props.arSceneNavigator.viroAppProps.maze;
-      for (let i = arr.length - 1; i >= 0; i--) {
-         for (let j = 0; j < arr[0].length; j++) {
-            initialX += 1;
-            let position = [initialX, 0, initialZ];
-            if (arr[i][j] === 1 || arr[i][j] === 2) {
-               render.push(
-                  <Viro3DObject
-                     source={require('./res/object_cube/object_cube.vrx')}
-                     resources={[
-                        require('./res/object_cube/cube_diffuse.png'),
-                        require('./res/object_cube/cube_specular.png'),
-                     ]}
-                     materials="hedge"
-                     scale={[1.0, 1.0, 1.0]}
-                     position={position}
-                     type="VRX"
-                     key={String.fromCharCode(i) + String.fromCharCode(j)}
-                  />,
-               );
-            } else if (arr[i][j] === 3) {
-               render.push(
-                  <Viro3DObject
-                     source={require('./res/coin/coin.vrx')}
-                     resources={[
-                        require('./res/coin/coin-texture.jpg'),
-                        require('./res/coin/img1.png'),
-                        require('./res/coin/img2.png'),
-                        require('./res/coin/img3.png'),
-                        require('./res/coin/gold.jpg'),
-                     ]}
-                     scale={[0.18, 0.18, 0.18]}
-                     position={position}
-                     type="VRX"
-                     key={String.fromCharCode(i) + String.fromCharCode(j)}
-                     animation={{ name: 'animateCoin', run: true, loop: true }}
-                     onClickState={() => {
-                        props.incrementCoins(i, j);
-                        arr[i][j] = 0;
-                     }}
-                     onDrag={() => {
-                        props.incrementCoins(i, j);
-                        arr[i][j] = 0;
-                     }}
-                  />,
-               );
-            }
-         }
-         initialX = -5;
-         initialZ--;
-      }
-      return render;
-   };
+  render() {
+    return (
+      <ViroARScene>
+        <ViroAmbientLight color="#ffffff" intensity={200} />
 
-   render() {
-      return (
-         <ViroARScene>
-            <ViroAmbientLight color="#ffffff" intensity={200} />
-
-            <ViroPortalScene
-               passable={true}
-               dragType="FixedDistance"
-               onDrag={() => {}}
-            >
-               <ViroPortal position={[0, 0, -2]} scale={[0.8, 0.8, 0.8]}>
-                  <Viro3DObject
-                     source={require('./ARPortals/portal_res/portal_archway/portal_archway.vrx')}
-                     resources={[
-                        require('./ARPortals/portal_res/portal_archway/portal_archway_diffuse.png'),
-                        require('./ARPortals/portal_res/portal_archway/portal_archway_normal.png'),
-                        require('./ARPortals/portal_res/portal_archway/portal_archway_specular.png'),
-                     ]}
-                     type="VRX"
-                  />
-               </ViroPortal>
-               <Viro360Image
-                  source={require('./ARPortals/portal_res/park.jpg')}
-               />
-               <ViroText
-                  fontSize={100}
-                  style={styles.boldFont}
-                  position={[0, 1.5, -4]}
-                  width={200}
-                  height={5}
-                  extrusionDepth={8}
-                  materials={['frontMaterial', 'backMaterial', 'sideMaterial']}
-                  text={String(this.state.time)}
-               />
-               {this.mazeGenerator()}
-            </ViroPortalScene>
-         </ViroARScene>
-      );
-   }
+        <ViroPortalScene
+          passable={true}
+          dragType="FixedDistance"
+          onDrag={() => {}}
+        >
+          <ViroPortal position={[0, 0, -2]} scale={[0.8, 0.8, 0.8]}>
+            <Viro3DObject
+              source={require('./ARPortals/portal_res/portal_archway/portal_archway.vrx')}
+              resources={[
+                require('./ARPortals/portal_res/portal_archway/portal_archway_diffuse.png'),
+                require('./ARPortals/portal_res/portal_archway/portal_archway_normal.png'),
+                require('./ARPortals/portal_res/portal_archway/portal_archway_specular.png'),
+              ]}
+              type="VRX"
+            />
+          </ViroPortal>
+          <Viro360Image source={require('./ARPortals/portal_res/park.jpg')} />
+          <ViroText
+            fontSize={100}
+            style={styles.boldFont}
+            position={[0, 1.5, -4]}
+            width={200}
+            height={5}
+            extrusionDepth={8}
+            materials={['frontMaterial', 'backMaterial', 'sideMaterial']}
+            text={String(this.state.time)}
+          />
+          {this.mazeGenerator()}
+        </ViroPortalScene>
+      </ViroARScene>
+    );
+  }
 }
 
 // Outside render function
 
 var styles = StyleSheet.create({
-   boldFont: {
-      color: '#FFFFFF',
-      flex: 1,
-      textAlignVertical: 'center',
-      textAlign: 'center',
-      fontWeight: 'bold',
-   },
+  boldFont: {
+    color: '#FFFFFF',
+    flex: 1,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
 });
 
 ViroMaterials.createMaterials({
-   frontMaterial: {
-      diffuseColor: '#FFFFFF',
-   },
-   backMaterial: {
-      diffuseColor: '#FF0000',
-   },
-   sideMaterial: {
-      diffuseColor: '#0000FF',
-   },
-   hedge: {
-      shininess: 2.0,
-      lightingModel: 'Blinn',
-   },
+  frontMaterial: {
+    diffuseColor: '#3734eb',
+  },
+  backMaterial: {
+    diffuseColor: '#3734eb',
+  },
+  sideMaterial: {
+    diffuseColor: '#2927b3',
+  },
 });
 ViroAnimations.registerAnimations({
-   animateCoin: {
-      properties: { rotateX: '+=360' },
-      easing: 'Linear',
-      duration: 6000,
-   },
+  animateCoin: {
+    properties: { rotateX: '+=360' },
+    easing: 'Linear',
+    duration: 6000,
+  },
 });
 
 module.exports = MainScene;
